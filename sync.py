c58ad2cc2c56a7782f4527a50948088d92c6e3ca
Max Malynowsky <github@maxmalynowsky.com>
2024-05-15 09:48:40 +0200

    initial commit

---
+++ sync.py
@@ -0,0 +1,24 @@
+import subprocess
+from pathlib import Path
+
+cwd = Path(__file__).parent
+
+
+def upload(src, dest):
+    subprocess.run(
+        [
+            "rclone",
+            "sync",
+            "--progress",
+            "--exclude=.*",
+            "--s3-no-check-bucket",
+            "--s3-chunk-size=256M",
+            src,
+            dest,
+        ]
+    )
+
+
+if __name__ == "__main__":
+    # upload(cwd / "dist/fonts", "r2://fieldmaps-data/fonts")
+    upload(cwd / "dist/styles", "r2://fieldmaps-data/styles")
